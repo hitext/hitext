3b4022e6751e2cae80b604119fbf0759ef388296
Roman Dvornov <rdvornov@gmail.com>
2018-06-27 02:08:32 +0300

    Add `match` helper

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,18 @@ function ensureArray(value) {
     return Array.isArray(value) ? value : [];
 }
 
+function match(source, origRx, cb) {
+    const flags = origRx.flags.indexOf('g') !== -1 ? origRx.flags : origRx.flags + 'g';
+    const rx = new RegExp(origRx, flags);
+    let m;
+
+    while (m = rx.exec(source)) {
+        cb(m.index, m.index + m[0].length, m);
+    }
+}
+
 module.exports = {
     ensureFunction,
-    ensureArray
+    ensureArray,
+    match
 };
