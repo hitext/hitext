9f43c3fc20dfbc1d3cfbbcc33a3c3240fba0df70
Roman Dvornov <rdvornov@gmail.com>
2018-07-04 23:33:59 +0300

    Remove match helper from utils

--- src/utils.js
+++ src/utils.js
@@ -8,18 +8,7 @@ function ensureArray(value) {
     return Array.isArray(value) ? value : [];
 }
 
-function match(source, origRx, cb) {
-    const flags = origRx.flags.indexOf('g') !== -1 ? origRx.flags : origRx.flags + 'g';
-    const rx = new RegExp(origRx, flags);
-    let m;
-
-    while (m = rx.exec(source)) {
-        cb(m.index, m.index + m[0].length, m);
-    }
-}
-
 module.exports = {
     ensureFunction,
-    ensureArray,
-    match
+    ensureArray
 };
