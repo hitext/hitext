81bd4e04ffb6b148d00b1d1e44139843b763231f
Roman Dvornov <rdvornov@gmail.com>
2020-05-14 16:56:32 +0200

    Update deps and build setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const { terser } = require('rollup-plugin-terser');
 
 module.exports = {
     input: 'src/index.js',
-    output: {
-        file: 'dist/hitext.js',
-        name: 'hitext',
-        format: 'umd'
-    },
+    output: [
+        { name: 'hitext', format: 'umd', file: 'dist/hitext.js' },
+        { name: 'hitext', format: 'umd', file: 'dist/hitext.min.js' }
+    ],
     plugins: [
         resolve({ browser: true }),
-        commonjs()
+        commonjs(),
+        terser({ include: /\.min\./ })
     ]
 };
