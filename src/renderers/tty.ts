a61af14984edd1327f38e39bac188364f84528a8
Roman Dvornov <rdvornov@gmail.com>
2025-10-15 04:53:53 +0200

    Refactor tty renderer

--- src/renderers/tty.ts
+++ src/renderers/tty.ts
@@ -1,12 +1,9 @@
 import type { RangeHookContext, RangeHooks, RangeHooksFactory } from '../types.js';
-import ansiStyles from 'ansi-styles';
 import { createRenderPipeline } from '../pipeline.js';
 import { StringBuffer } from '../string-buffer.js';
 
-const initialStyle = createStyle('reset');
-
-type ForegroundColorName = keyof ansiStyles.ForegroundColor;
-type BackgroundColorName = keyof ansiStyles.BackgroundColor;
+type ForegroundColorName = keyof typeof styles.color;
+type BackgroundColorName = keyof typeof styles.bgColor;
 type StyleMod = ForegroundColorName | BackgroundColorName | 'reset';
 type StyleModMap = { [key: string]: StyleMod | StyleMod[] } | Array<StyleMod | StyleMod[]>;
 type Style = {
@@ -20,23 +17,55 @@ type RangeHooksFactoryContext = {
     popStyle: () => void;
 }
 
-function isForegroundColor(name: StyleMod): name is ForegroundColorName {
-    return name in ansiStyles.color;
-}
-
-function isBackgroundColor(name: StyleMod): name is BackgroundColorName {
-    return name in ansiStyles.bgColor;
-}
+const initialStyle = /* @__PURE__ */ createStyle('reset');
+const styles = {
+    color: {
+        black: 30,
+        red: 31,
+        green: 32,
+        yellow: 33,
+        blue: 34,
+        magenta: 35,
+        cyan: 36,
+        white: 37,
+        blackBright: 90,
+        redBright: 91,
+        greenBright: 92,
+        yellowBright: 93,
+        blueBright: 94,
+        magentaBright: 95,
+        cyanBright: 96,
+        whiteBright: 97
+    },
+    bgColor: {
+        bgBlack: 40,
+        bgRed: 41,
+        bgGreen: 42,
+        bgYellow: 43,
+        bgBlue: 44,
+        bgMagenta: 45,
+        bgCyan: 46,
+        bgWhite: 47,
+        bgBlackBright: 100,
+        bgRedBright: 101,
+        bgGreenBright: 102,
+        bgYellowBright: 103,
+        bgBlueBright: 104,
+        bgMagentaBright: 105,
+        bgCyanBright: 106,
+        bgWhiteBright: 107
+    }
+};
 
 function createStyle(...style: StyleMod[]): Style {
     return style.reduce((result: Style, name) => {
         if (name === 'reset') {
             result.color = '\u001B[39m';
             result.bgColor = '\u001B[49m';
-        } else if (isForegroundColor(name)) {
-            result.color = ansiStyles.color[name].open;
-        } else if (isBackgroundColor(name)) {
-            result.bgColor = ansiStyles.bgColor[name].open;
+        } else if (Object.hasOwn(styles.color, name)) {
+            result.color = `\u001B[${styles.color[name as ForegroundColorName]}m`;
+        } else if (Object.hasOwn(styles.bgColor, name)) {
+            result.bgColor = `\u001B[${styles.bgColor[name as BackgroundColorName]}m`;
         }
 
         return result;
@@ -63,91 +92,88 @@ function rangeHooksFactoryCreateStyleMap(map: StyleModMap, fetcher = ({ data }: 
     return (context: RangeHookContext<any>) => styleMap[fetcher(context)];
 }
 
-function _styleToRender(current: Style, next: Style = {}) {
-    let modifiers = '';
+export const createTTYRenderer = /* @__PURE__ */ Object.assign(
+    function createTTYRenderer<RenderOptions>() {
+        return createRenderPipeline<RenderOptions, string, string, RangeHooksFactoryContext>(() => {
+            const stack: Style[] = [];
+            let currentStyle: Style = initialStyle;
+            let renderedStyle: Style = {};
 
-    for (const key in current) {
-        const styleKey = key as keyof Style;
-        if (current[styleKey] !== next[styleKey]) {
-            const nextValue = next[styleKey];
-            if (nextValue) {
-                modifiers += nextValue;
-            }
-        }
-    }
+            return {
+                createBuffer: () => new StringBuffer(),
+                open: styleToRender,
+                close: styleToRender,
+                text: (sourceChunk) => styleToRender() + sourceChunk,
 
-    return modifiers;
-}
-
-export function createTTYRenderer<RenderOptions>() {
-    return createRenderPipeline<RenderOptions, string, string, RangeHooksFactoryContext>(() => {
-        const stack: Style[] = [];
-        let currentStyle: Style = initialStyle;
-        let renderedStyle = {};
-
-        return {
-            createBuffer: () => new StringBuffer(),
-            open: styleToRender,
-            close: styleToRender,
-            text: (sourceChunk) => styleToRender() + sourceChunk,
-
-            // Provide style utils to range hooks factories
-            rangeHooksContext: {
-                createStyle: wrapToContext(rangeHooksFactoryCreateStyle),
-                createStyleMap: wrapToContext(rangeHooksFactoryCreateStyleMap),
-                pushStyle,
-                popStyle
-            }
-        };
-
-        function pushStyle(style: Style) {
-            stack.push(currentStyle);
-            currentStyle = Object.assign({}, currentStyle, style);
-        }
-        function popStyle() {
-            currentStyle = stack.pop() || currentStyle;
-        }
-        function styleToRender() {
-            if (renderedStyle !== currentStyle) {
-                const newStyle = _styleToRender(renderedStyle, currentStyle);
-
-                renderedStyle = currentStyle || {};
-
-                if (newStyle) {
-                    return newStyle;
+                // Provide style utils to range hooks factories
+                rangeHooksContext: {
+                    createStyle: rangeHooksFactory(rangeHooksFactoryCreateStyle),
+                    createStyleMap: rangeHooksFactory(rangeHooksFactoryCreateStyleMap),
+                    pushStyle,
+                    popStyle
                 }
+            };
+
+            function pushStyle(style: Style) {
+                stack.push(currentStyle);
+                currentStyle = Object.assign({}, currentStyle, style);
             }
+            function popStyle() {
+                currentStyle = stack.pop() || currentStyle;
+            }
+            function styleToRender() {
+                if (renderedStyle !== currentStyle) {
+                    let newStyle = '';
 
-            return '';
-        }
-        function wrapToContext<T extends(...args: any[]) => any>(fn: T) {
-            return (...args: Parameters<T>): Partial<RangeHooks<any, any>> => {
-                const styleFetcher = fn(...args);
-
-                return {
-                    open(context) {
-                        pushStyle(styleFetcher(context) || {});
-                        return '';
-                    },
-                    close() {
-                        popStyle();
-                        return '';
+                    for (const key in renderedStyle) {
+                        const styleKey = key as keyof Style;
+                        if (renderedStyle[styleKey] !== currentStyle[styleKey]) {
+                            const nextValue = currentStyle[styleKey];
+                            if (nextValue) {
+                                newStyle += nextValue;
+                            }
+                        }
                     }
+
+                    renderedStyle = currentStyle || {};
+
+                    if (newStyle !== '') {
+                        return newStyle;
+                    }
+                }
+
+                return '';
+            }
+            function rangeHooksFactory<T extends(...args: any[]) => any>(fn: T) {
+                return (...args: Parameters<T>): Partial<RangeHooks<any, any>> => {
+                    const styleFetcher = fn(...args);
+
+                    return {
+                        open(context) {
+                            pushStyle(styleFetcher(context) || {});
+                            return '';
+                        },
+                        close() {
+                            popStyle();
+                            return '';
+                        }
+                    };
                 };
             };
-        };
-    });
-}
+        });
+    },
+    // Attach helper functions as static methods to the factory function itself
+    {
+        createStyle: (
+            ...args: Parameters<RangeHooksFactoryContext['createStyle']>
+        ): RangeHooksFactory<any, any, any, RangeHooksFactoryContext> => ({
+            createRangeHooks: ({ createStyle }) => createStyle(...args)
+        }),
 
-// Attach helper functions as static methods to the factory function itself
-createTTYRenderer.createStyle = (
-    ...args: Parameters<RangeHooksFactoryContext['createStyle']>
-): RangeHooksFactory<any, any, any, RangeHooksFactoryContext> => ({
-    createRangeHooks: ({ createStyle }) => createStyle(...args)
-});
-
-createTTYRenderer.createStyleMap = (
-    ...args: Parameters<RangeHooksFactoryContext['createStyleMap']>
-): RangeHooksFactory<any, any, any, RangeHooksFactoryContext> => ({
-    createRangeHooks: ({ createStyleMap }) => createStyleMap(...args)
-});
+        createStyleMap: (
+            ...args: Parameters<RangeHooksFactoryContext['createStyleMap']>
+        ): RangeHooksFactory<any, any, any, RangeHooksFactoryContext> => ({
+            createRangeHooks: ({ createStyleMap }) => createStyleMap(...args)
+        })
+    }
+);
